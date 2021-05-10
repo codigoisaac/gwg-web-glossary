a93b55ce268f7c14adb0b6a5867b39ea33e8f51a
Isaac Muniz <codigoisaac@protonmail.com>
2021-05-10 17:09:09 -0300

    Include and Require in PHP

---
+++ 2.phpLanguage/2.8.include.php
@@ -0,0 +1,22 @@
+<?php
+$title = 'include';
+
+include('./../inc/header.php');
+require('./../inc/functions.php'); // show error if not found
+
+$alchemies = [
+	['name' => 'Fruitarianism', 'power' => 'Energy'],
+	['name' => 'Orin Therapy', 'power' => 'Healing'],
+	['name' => 'Semen Retention', 'power' => 'Well Being'],
+];
+
+$res = array_column($alchemies, 'power');
+?>
+
+<h2>My Alchemy Practices</h2>
+
+<?php
+output($res);
+?>
+
+<?php include('./../inc/footer.php');
