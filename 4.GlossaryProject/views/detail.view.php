40a0ad847f1e97ee05cd713c9904be4a6629bad7
Isaac Muniz <codigoisaac@protonmail.com>
2021-05-20 20:33:45 -0300

    Update detail.view.php

--- 4.GlossaryProject/views/detail.view.php
+++ 4.GlossaryProject/views/detail.view.php
@@ -2,7 +2,7 @@
 	<div class="row">
 		<div class="col-lg-12 text-center">
 			<h1 class="mt-5"><?= $model->term ?></h1>
-			<h2><?= $model->definition ?></h2>
+			<p><?= $model->definition ?></p>
 		</div>
 	</div>
 </div>
